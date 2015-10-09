6de1e3e2bd288648b1e26606401426230ed0be6f
leizi2011 <leizi2011@users.noreply.github.com>
2015-10-09 14:22:44 +0800

    Update AndroidContactsTest.java

--- cmcc/src/main/java/foo/AndroidContactsTest.java
+++ cmcc/src/main/java/foo/AndroidContactsTest.java
@@ -1,6 +1,7 @@
 package foo;
-
+//junit实现方式
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -28,14 +29,13 @@ public class AndroidContactsTest {
 		File appDir = new File(classpathRoot, "apps/ContactManger");
 		File app = new File(appDir, "Calculator.apk");
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("deviceName", "emulator-5554");
+		capabilities.setCapability("deviceName", "emulator-5554");//设备名称
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
-		capabilities.setCapability("platformVersion", "4.4.2");
-		capabilities.setCapability("platformName", "Android");
-		capabilities.setCapability("app", app.getAbsolutePath());
-		System.out.println(app.getAbsolutePath());
-		capabilities.setCapability("app-package", "com.android.calculator2");
-		capabilities.setCapability("app-activity", ".Calculator");
+		capabilities.setCapability("platformVersion", "4.4.2");//平台版本
+		capabilities.setCapability("platformName", "Android");//平台名称
+		capabilities.setCapability("app", app.getAbsolutePath());//应用的绝对路径
+		capabilities.setCapability("app-package", "com.android.calculator2");//app的包名
+		capabilities.setCapability("app-activity", ".Calculator");//app的activity，原生的需要在其activity前面加.;
 		driver=new AppiumDriver(new  URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		
 //		driver = new SwipeableWebDriver(
@@ -48,8 +48,21 @@ public class AndroidContactsTest {
 	}
 
 	@Test
-	public void addContact() {
-		System.out.println(1111);
+	public void addContact() {		
+		WebElement e1=driver.findElementById("com.android.calculator2:id/digit9");
+		e1.click();
+		WebElement e3=driver.findElementById("com.android.calculator2:id/plus");
+		e3.click();
+		WebElement e2=driver.findElementById("com.android.calculator2:id/digit6");
+		e2.click();
+		WebElement e4=driver.findElementById("com.android.calculator2:id/equal");
+		e4.click();
+		WebElement e5=driver.findElementByClassName("android.widget.EditText");
+		String s=e5.getText();
+		Assert.assertEquals(s, "15");
+		//Assert.assertEquals(s, "16");
+		//System.out.println(1111);
+		
 //		WebElement el = driver.findElement(By.name("Add Contact"));
 //		el.click();
 //		List<WebElement> textFieldsList = driver.findElements(By
@@ -75,3 +88,5 @@ public class AndroidContactsTest {
 	// }
 	// }
 }
+
+
